9070b2edc4692eebcdb4a4317df88fbeadf04b2e
Paweł Galiński <pawel.galinski@gmail.com>
2016-01-03 19:20:40 +0100

    Graphic engine added

---
+++ Kaboom/KaboomApp/src/pl/pg/kaboom/activity/GameMainActivity.java
@@ -0,0 +1,15 @@
+package pl.pg.kaboom.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import pl.pg.kaboom.R;
+
+public class GameMainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_main);
+    }
+}
